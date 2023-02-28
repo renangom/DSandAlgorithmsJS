9c5e53177d19e42e6245f78dec5585184931b295
Renan <renan23eduardo@gmail.com>
2023-02-28 09:52:23 -0300

    Create an algorithm to convert decimal to binary

---
+++ stacks/DecimalToBinary.js
@@ -0,0 +1,16 @@
+function decimalToBinary(decNumber) {
+    const remStack = new Stack();
+    let number = decNumber;
+    let rem;
+    let binaryString = '';
+    while(number > 0) {
+        rem = Math.floor(number % 2);
+        remStack.push(rem);
+        number = Math.floor(number / 2);
+    }
+    while (!remStack.isEmpty()){
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
+}
--- stacks/stack2.js
+++ stacks/stack2.js
@@ -61,3 +61,8 @@ class Stack{
     }
     
 }
+
+const stack = new Stack();
+console.log(Object.getOwnPropertyNames(stack));
+console.log(Object.keys(stack));
+console.log(stack.items);
